f87e34e515cb6f411987b02c4c7b255219b05b51
szkuramoto <sleepqngel@gmail.com>
2024-10-15 20:56:52 -0300

    finalização do portfólio
    finalização do portfólio

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,1 +1,18 @@
-//alert('hello');
+/* ===== transformar o menu hamburguer em um X */
+const menuHamburguer = document.querySelector('.menu-hamburguer');
+menuHamburguer.addEventListener('click', () => {
+   toggleMenu();
+});
+
+/*função pra trazer menu */
+function toggleMenu() {
+    const nav = document.querySelector('.nav-responsive');
+    menuHamburguer.classList.toggle('change');
+
+    if (menuHamburguer.classList.contains('change')){
+        nav.style.display = 'block';
+
+    } else {
+        nav.style.display = 'none';
+    }
+}
